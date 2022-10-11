e7cf57f681f34a07f8109199f896ca91f8e4bccd
Sabina Saad <sabinasaad@MacBook-Air-Sabina.local>
2022-10-11 12:40:45 +0300

    Added feature: counting rounds and game over. GuessTheWord 1.0 is complete

--- src/game/GuessTheWord.java
+++ src/game/GuessTheWord.java
@@ -4,33 +4,52 @@ import java.util.Scanner;
 
 public class GuessTheWord {
 
+    Scanner scanner = new Scanner(System.in);
     private boolean play = true;
     private Words randomWord = new Words();
-    Scanner scanner = new Scanner(System.in);
+    private int rounds = 10;
+    private char lastRound;
+
     public void start() {
 
         do {
             showWord();
             getInput();
             checkInput();
-        }
-        while(play);
+        } while (play);
     }
 
     private void showWord() {
+        System.out.println("You have " + rounds + " tries left.");
         System.out.println(randomWord);
     }
 
     private void getInput() {
         System.out.println("Enter a letter to guess the word");
         String userGuess = scanner.nextLine();
-        char letter;
-        randomWord.guess(userGuess.charAt(0));
+        lastRound = userGuess.charAt(0);
+
     }
 
     private void checkInput() {
 
-        System.out.println("checkinput");
+        boolean isGuessedRight = randomWord.guess(lastRound);
+
+        if (isGuessedRight) {
+
+            if (randomWord.isGuessedRight()) {
+                System.out.println("Congratulations! You won!");
+                System.out.println("The word is " + randomWord);
+                play = false;
+            }
+        } else {
+            rounds--;
+
+            if (rounds == 0) {
+                System.out.println("Game over!");
+                play = false;
+            }
+        }
     }
 
     public void end() {
--- src/game/Words.java
+++ src/game/Words.java
@@ -4,32 +4,18 @@ import java.util.Random;
 
 public class Words {
 
-    private final String[] randomWords = {
-            "animals",
-            "happiness",
-            "indefinite",
-            "steady",
-            "birthday",
-            "extreme",
-            "rights",
-            "properties",
-            "ceremony",
-            "independence",
-            "beneath",
-            "information",
-            "students",
-            "employee"
-    };
-    private String selectedWord;
-    private Random random = new Random();
-    private char[] letters;
+    private final String[] randomWords = {"animals", "happiness", "indefinite", "steady", "birthday", "extreme",
+            "rights", "properties", "ceremony", "independence", "beneath", "information", "students", "employee"};
+    private final String selectedWord;
+    private final Random random = new Random();
+    private final char[] letters;
 
     public Words() {
         selectedWord = randomWords[random.nextInt(randomWords.length)];
         letters = new char[selectedWord.length()];
     }
 
-    public String toString(){
+    public String toString() {
         StringBuilder text = new StringBuilder();
         for (char letter : letters) {
             text.append(letter == '\u0000' ? '-' : letter);
@@ -39,21 +25,26 @@ public class Words {
         return text.toString();
     }
 
-    public boolean isGuessedRight(){
+    public boolean isGuessedRight() { //whole word
 
         for (char letter : letters) {
-            if (letter == '\u0000'){
+            if (letter == '\u0000') {
                 return false;
             }
         }
         return true;
     }
-    public void guess(char letter) {
 
-        for (int i = 0; i < selectedWord.length(); i++){
+    public boolean guess(char letter) {
+
+        boolean guessedRight = false;
+
+        for (int i = 0; i < selectedWord.length(); i++) {
             if (letter == selectedWord.charAt(i)) {
                 letters[i] = letter;
+                guessedRight = true;
             }
         }
+        return guessedRight;
     }
 }
